45a6eb499f61bff9dac28f51e7d8b516428f295b
Igor Souza <igor.souza.webmaster@gmail.com>
2019-07-06 16:14:34 -0300

    Adiciona estilo global e assets que serao usados futuramente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    </>
   );
 }
 
